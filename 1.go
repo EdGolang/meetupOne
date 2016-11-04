1144533a961e701516f8ac140df03181d0a8a784
Cameron MacLeod <cmacleod170@googlemail.com>
2016-11-04 13:52:28 +0000

    State at end of meetup

--- 1.go
+++ 1.go
@@ -6,8 +6,27 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
 import (
+	"encoding/json"	
 	"net/http"
 )
 
 func EulerOneHandler(rw http.ResponseWriter, req *http.Request) {
+    sum := findSum(1000)
+	responseData := map[string]interface{} {
+		"result" : "OK",
+        "sum": sum,
+	}
+
+	jsonEncoder := json.NewEncoder(rw)
+	jsonEncoder.Encode(responseData)
+}
+
+func findSum(under int) int {
+    sum := 0
+    for i := 1; i < under; i++ {
+        if i % 3 == 0 || i % 5 == 0 {
+            sum += i
+        }
+    }
+    return sum
 }
--- server.go
+++ server.go
@@ -17,18 +17,18 @@ func indexHandler (rw http.ResponseWriter, req *http.Request) {
 
 func main() {
 	http.HandleFunc("/", indexHandler);
-	
+
 	http.HandleFunc("/1", EulerOneHandler);
 	http.HandleFunc("/2", EulerTwoHandler);
 	http.HandleFunc("/3", EulerThreeHandler);
-	http.HandleFunc("/4", EulerFourHandler);
-	http.HandleFunc("/5", EulerFiveHandler);
-	http.HandleFunc("/6", EulerSixHandler);
-	http.HandleFunc("/7", EulerSevenHandler);
-	http.HandleFunc("/8", EulerEightHandler);
-	http.HandleFunc("/9", EulerNineHandler);
-	http.HandleFunc("/10", EulerTenHandler);
-	
+//	http.HandleFunc("/4", EulerFourHandler);
+//	http.HandleFunc("/5", EulerFiveHandler);
+//	http.HandleFunc("/6", EulerSixHandler);
+//	http.HandleFunc("/7", EulerSevenHandler);
+//	http.HandleFunc("/8", EulerEightHandler);
+//	http.HandleFunc("/9", EulerNineHandler);
+//	http.HandleFunc("/10", EulerTenHandler);
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err);
 	}
